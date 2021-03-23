da9db1637bfdfbdcc47b8e761437432727f118be
PriteshLunkad <75626361+PriteshLunkad@users.noreply.github.com>
2021-03-23 12:34:15 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='speech_to_text',
+    packages=find_packages(),
+    version=’0.1.0',
+    description=’This library converts speech to text and makes conversions to speech’,
+    author='Pritesh Lunkad',
+    license=’MIT’,
+)
